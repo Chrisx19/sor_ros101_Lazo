a9615842ff6ed1e0831d048a3e964c93dfabccde
Chrisx19 <107272321+Chrisx19@users.noreply.github.com>
2022-10-20 22:42:00 -0700

    Update joy_turtle.py

--- ros_turtlesim/scripts/joy_turtle.py
+++ ros_turtlesim/scripts/joy_turtle.py
@@ -3,14 +3,21 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
-def joy_callback(msg_joy):
-    vel = Twist()
-    
-    vel.linear.x = 1 * msg_joy.axes[1]
-    vel.angular.z = 3 * msg_joy.axes[3]
-    vel_pub.publish(vel)
-    
-rospy.init_node('joy_turtle')
-joy_sub = rospy.Subscriber("/joy", Joy, joy_callback)
-vel_pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size = 10)
-rospy.spin()
+class Joystick(object):
+    def __init__(self):
+        rospy.loginfo("Starting joystick for turtle")
+
+        self.cmd_pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
+        self.joy_sub = rospy.Subscriber("/joy", Joy, self.joy_cb, queue_size=10)
+
+    def joy_cb(self, joy_msg):
+        self.vel = Twist()
+
+        self.vel.linear.x = 2 * joy_msg.axes[1]
+        self.vel.angular.z = 3 * joy_msg.axes[3]
+        self.cmd_pub.publish(self.vel)
+
+if __name__ == "__main__":
+    rospy.init_node("Turtle_Joy_Node", anonymous=False)
+    joy = Joystick()
+    rospy.spin()
