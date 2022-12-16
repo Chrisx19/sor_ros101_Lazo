4f33cdd7ed2af853686f6cd4752df0b12cec4566
Chrisx19 <107272321+Chrisx19@users.noreply.github.com>
2022-12-16 10:47:26 -0800

    Update square_turtle.py

--- ros_turtlesim/scripts/square_turtle.py
+++ ros_turtlesim/scripts/square_turtle.py
@@ -5,28 +5,28 @@ import math
 from geometry_msgs.msg import Twist             #velocity package for command velocity
 
 
-rospy.init_node('square_turtle')
-publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+rospy.init_node('event2')
+publisher = rospy.Publisher('/cmd_vel_AV', Twist, queue_size=1)
 
 msg = Twist()   #constructor
 
-while not rospy.is_shutdown():
+# while not rospy.is_shutdown():
     # Move forward          When sending value, it has to be continues so that robot dont stop
-    start_time = time.time()
-    while ( (time.time() - start_time) < 1):   #one second to go forward
-        msg.linear.x = 1          #m/s
-        msg.angular.z = 0.0         #rad/sec
-        publisher.publish(msg)
-        time.sleep(0.1)             #before stopping, it will have that wait delay
-    msg.linear.x = 0.0              #This is the stop, so it goes for 1 second then stop
+start_time = time.time()
+while ( (time.time() - start_time) < 5):   #one second to go forward
+    msg.linear.x = 63          #m/s
+    msg.angular.z = 0.0         #rad/sec
     publisher.publish(msg)
+    time.sleep(0.1)             #before stopping, it will have that wait delay
+msg.linear.x = 0.0              #This is the stop, so it goes for 1 second then stop
+publisher.publish(msg)
 
-        # Turn
-    start_time = time.time()
-    while (time.time() - start_time) < 1:   #one second to turn
-        msg.linear.x = 0.0                      #m/s
-        msg.angular.z = -1*math.pi/2.0         #rad/sec  pi/2 on unit circle
-        publisher.publish(msg)
-        time.sleep(0.1)
-    msg.angular.z = 0.0                         #This is the stop, so it turns for 1 second then stop      
-    publisher.publish(msg)
+    # Turn
+# start_time = time.time()
+# while (time.time() - start_time) < 1:   #one second to turn
+#     msg.linear.x = 0.0                      #m/s
+#     msg.angular.z = -1*math.pi/2.0         #rad/sec  pi/2 on unit circle
+#     publisher.publish(msg)
+#     time.sleep(0.1)
+# msg.angular.z = 0.0                         #This is the stop, so it turns for 1 second then stop      
+# publisher.publish(msg)
