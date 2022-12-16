b601be142c8d13143302ac9caba48319301bf192
Chrisx19 <107272321+Chrisx19@users.noreply.github.com>
2022-12-16 11:48:02 -0800

    Update square_turtle.py

--- ros_turtlesim/scripts/square_turtle.py
+++ ros_turtlesim/scripts/square_turtle.py
@@ -15,16 +15,24 @@ msg = Twist()   #constructor
 start_time = time.time()            #stright
 while ( (time.time() - start_time) < 5):   #one second to go forward
     msg.linear.x = 63          #m/s
-    msg.angular.z = 0.0         #rad/sec
+    msg.angular.z = -100.0         #rad/sec
     publisher.publish(msg)
     time.sleep(0.1)             #before stopping, it will have that wait delay
 publisher.publish(msg)
 
     # Turn
 start_time = time.time()
-while (time.time() - start_time) < 1:   #one second to turn
+while (time.time() - start_time) < 5:   #one second to turn
+    msg.linear.x = 55                      #m/s
+    msg.angular.z = 500           #rad/sec  pi/2 on unit circle
+    publisher.publish(msg)
+    time.sleep(0.1)
+publisher.publish(msg)
+
+start_time = time.time()
+while (time.time() - start_time) < 5:   #one second to turn
     msg.linear.x = 63                      #m/s
-    msg.angular.z = -350           #rad/sec  pi/2 on unit circle
+    msg.angular.z = 100           #rad/sec  pi/2 on unit circle
     publisher.publish(msg)
     time.sleep(0.1)
 msg.linear.x = 0
